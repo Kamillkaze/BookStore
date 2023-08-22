7571ed7df5c70c34af68c59dd4fe89bec8c45a9c
Kamil Tomanek <kamilttt271@gmail.com>
2023-08-22 15:42:11 +0200

    Use apache commons library to remove accents from author and title while generating urlId for Book

--- model/src/main/java/com/bookstore/utils/UrlIdGenerator.java
+++ model/src/main/java/com/bookstore/utils/UrlIdGenerator.java
@@ -1,8 +1,13 @@
 package com.bookstore.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class UrlIdGenerator {
     public static String generate(String author, String title) {
-        return (author.toLowerCase() + "-" + title.toLowerCase())
-                        .replace(' ', '-');
+        // Doesn't convert "ł"
+        String urlId = (author.toLowerCase() + "-" + title.toLowerCase())
+                .replace(' ', '-');
+
+        return StringUtils. stripAccents(urlId);
     }
 }
