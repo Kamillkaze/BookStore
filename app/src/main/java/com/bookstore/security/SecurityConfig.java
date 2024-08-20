2928c9a8a8ad55845ec9070a81fc3c8dcd934421
Kamil Tomanek <kamilttt271@gmail.com>
2024-08-20 08:37:47 +0200

    Add basic filter chain

--- app/src/main/java/com/bookstore/security/SecurityConfig.java
+++ app/src/main/java/com/bookstore/security/SecurityConfig.java
@@ -1,8 +1,39 @@
 package com.bookstore.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(customizer ->
+                customizer
+                        .requestMatchers(HttpMethod.GET, "api/v1/").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "api/v1/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "api/v1/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "api/v1/").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 }
