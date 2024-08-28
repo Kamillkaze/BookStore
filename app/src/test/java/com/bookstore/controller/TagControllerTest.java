5decefe8a906725aa1848c39725ffefb2ad5ccdd
Kamil Tomanek <kamilttt271@gmail.com>
2024-08-28 07:52:00 +0200

    Add user auth mocking to TagControllerTest

--- app/src/test/java/com/bookstore/controller/TagControllerTest.java
+++ app/src/test/java/com/bookstore/controller/TagControllerTest.java
@@ -1,28 +1,44 @@
 package com.bookstore.controller;
 
 import com.bookstore.dto.TagDto;
+import com.bookstore.security.SecurityConfig;
 import com.bookstore.service.TagService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.sql.DataSource;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@Import(SecurityConfig.class)
 @WebMvcTest(controllers = TagController.class)
 class TagControllerTest {
+
+    @TestConfiguration
+    static class MockDataSourceConfig {
+        @Bean
+        @Primary
+        public DataSource dataSource() {
+            return mock(DataSource.class);
+        }
+    }
 
     @MockBean
     private TagService tagService;
@@ -35,6 +51,7 @@ class TagControllerTest {
 
     @Test
     @DisplayName("Should return error when trying to add an entity with already existing name addNewTag()")
+    @WithMockUser(roles = "ADMIN")
     void addNewTagWhenDuplicatedId() throws Exception {
         String tagName = "tag1";
         String errorMessage = "could not execute statement [Duplicate entry";
@@ -52,6 +69,7 @@ class TagControllerTest {
 
     @Test
     @DisplayName("Should add a new tag correctly addNewTag()")
+    @WithMockUser(roles = "ADMIN")
     void addNewTagCorrect() throws Exception {
         String tagName = "tag1";
         TagDto tagDto = new TagDto(tagName, 0L);
@@ -68,6 +86,7 @@ class TagControllerTest {
 
     @Test
     @DisplayName("Should return error code when trying to delete non-existing tag deleteATag()")
+    @WithMockUser(roles = "ADMIN")
     void deleteATagWhenNonExistingTag() throws Exception {
         String tagName = "tag1";
         String expected = "An object with specified property does not exist";
@@ -85,6 +104,7 @@ class TagControllerTest {
 
     @Test
     @DisplayName("Should delete a tag correctly deleteATag()")
+    @WithMockUser(roles = "ADMIN")
     void deleteATagCorrect() throws Exception {
         String tagName = "tag1";
         String expected = "Tag \"" + tagName + "\" successfully removed.";
@@ -99,6 +119,7 @@ class TagControllerTest {
 
     @Test
     @DisplayName("Should return a list of all tags correctly getAll()")
+    @WithMockUser
     void getAllTags() throws Exception {
         List<TagDto> tagDtos = List.of(new TagDto("tag1", 5L), new TagDto("tag2", 1L));
         String expected = objectMapper.writeValueAsString(tagDtos);
