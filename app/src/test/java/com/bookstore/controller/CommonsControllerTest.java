888e6e2de375694251def1add3ee4f5e9cd637ee
Kamil Tomanek <kamilttt271@gmail.com>
2024-08-28 07:31:52 +0200

    Add user auth mocking to CommonsControllerTest

--- app/src/test/java/com/bookstore/controller/CommonsControllerTest.java
+++ app/src/test/java/com/bookstore/controller/CommonsControllerTest.java
@@ -1,25 +1,43 @@
 package com.bookstore.controller;
 
 import com.bookstore.dto.TagDto;
+import com.bookstore.security.SecurityConfig;
 import com.bookstore.service.CommonsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.sql.DataSource;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@Import(SecurityConfig.class)
 @WebMvcTest(controllers = CommonsController.class)
 class CommonsControllerTest {
+
+    @TestConfiguration
+    static class MockDataSourceConfig {
+        @Bean
+        @Primary
+        public DataSource dataSource() {
+            return mock(DataSource.class);
+        }
+    }
 
     @MockBean
     private CommonsService commonsService;
@@ -32,6 +50,7 @@ class CommonsControllerTest {
 
     @Test
     @DisplayName("Should handle NoSuchElementException thrown in service addBookToTag()")
+    @WithMockUser(roles = "ADMIN")
     void addBookToTagWhenNoSuchElementThrown() throws Exception {
         String tagName = "tag1";
         String bookUrlId = "url-id";
@@ -48,6 +67,7 @@ class CommonsControllerTest {
 
     @Test
     @DisplayName("Should handle lack of bookUrlId parameter addBookToTag()")
+    @WithMockUser(roles = "ADMIN")
     void addBookToTagWhenNoBookUrlId() throws Exception {
         String tagName = "tag1";
 
@@ -61,6 +81,7 @@ class CommonsControllerTest {
 
     @Test
     @DisplayName("Should add book to tag correctly addBookToTag()")
+    @WithMockUser(roles = "ADMIN")
     void addBookToTagCorrect() throws Exception {
         String tagName = "tag1";
         String bookUrlId = "url-id";
@@ -79,6 +100,7 @@ class CommonsControllerTest {
 
     @Test
     @DisplayName("Should handle NoSuchElementException thrown in service removeBookFromTag()")
+    @WithMockUser(roles = "ADMIN")
     void removeBookFromTagWhenNoSuchElementThrown() throws Exception {
         String tagName = "tag1";
         String bookUrlId = "url-id";
@@ -95,6 +117,7 @@ class CommonsControllerTest {
 
     @Test
     @DisplayName("Should handle lack of bookUrlId parameter removeBookFromTag()")
+    @WithMockUser(roles = "ADMIN")
     void removeBookFromTagWhenNoBookUrlId() throws Exception {
         String tagName = "tag1";
 
@@ -109,6 +132,7 @@ class CommonsControllerTest {
 
     @Test
     @DisplayName("Should delete a book from tag correctly removeBookFromTag()")
+    @WithMockUser(roles = "ADMIN")
     void removeBookFromTagCorrect() throws Exception {
         String tagName = "tag1";
         String bookUrlId = "url-id";
