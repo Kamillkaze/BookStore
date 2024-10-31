d91fc9f67b47e735830b847a8076bf59b18e43e6
Kamil <kamilttt271@gmail.com>
2024-10-31 13:58:58 +0100

    Add support for dotenv

--- app/src/main/java/com/bookstore/BookStoreApplication.java
+++ app/src/main/java/com/bookstore/BookStoreApplication.java
@@ -2,10 +2,13 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BookStoreApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(BookStoreApplication.class);
     }
 }
